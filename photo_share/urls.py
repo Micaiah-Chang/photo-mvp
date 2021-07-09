fb5278f2368650eacb51edf5564d8b65412b1168
Alan Chang <ahchang012@gmail.com>
2021-07-08 17:19:52 -0700

    Add additional views
    Added the individual album + photo views, added photo + album association.

--- photo_share/urls.py
+++ photo_share/urls.py
@@ -6,5 +6,10 @@ from . import views
 
 urlpatterns = [
     path(r'photos/', views.PhotoView.as_view(), name='photo_view'),
-    path(r'albums/', views.AlbumView.as_view(), name='album_view')
+    path(r'photos/<int:photo_id>/', views.IndividualPhotoView.as_view(), name='single_photo_view'),
+    path(r'albums/', views.AlbumView.as_view(), name='album_view'),
+    path(r'albums/<int:album_id>/', views.IndividualAlbumView.as_view(),
+         name='single_album_view'),
+    path(r'albums/<int:album_id>/add-photo/<int:photo_id>/', views.AddPhotoView.as_view(),
+         name='add_photo'),
 ]
--- photo_share/views.py
+++ photo_share/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -35,6 +35,25 @@ class PhotoView(APIView):
             errors = serializer.errors
             return Response(errors, status=rest_status.HTTP_400_BAD_REQUEST)
 
+class IndividualPhotoView(APIView):
+
+
+    def get(self, request, photo_id):
+        user = request.user
+
+        photo = get_object_or_404(models.Photo, user=user, id=photo_id)
+        serializer = serializers.PhotoSerializer(photo)
+        return Response(serializer.data)
+
+    def delete(self, request, photo_id):
+        user, data = request.user, request.data
+        photo = models.Photo.objects.filter(user=user, id=photo_id)
+        if photo.exists():
+            photo.delete()
+            return Response({}, status=rest_status.HTTP_204_NO_CONTENT)
+        else:
+            error = {"detail": "Photo does not exist."}
+            return Response(error, status=rest_status.HTTTP_404_NOT_FOUND)
 
 class AlbumView(APIView):
 
@@ -66,25 +85,27 @@ class IndividualAlbumView(APIView):
     def patch(self, request, album_id):
         user = request.user
         album = models.Album.objects.get(user=user, id=album_id)
+        serializer = serializers.AlbumSerializer(album, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save(user=user)
-            return Response(errors, status=rest_status.HTTP_200_ACCEPTED)
+            return Response(serializer.datax, status=rest_status.HTTP_200_OK)
         else:
             errors = serializer.errors
             return Response(errors, status=rest_status.HTTP_400_BAD_REQUEST)
 
 class AddPhotoView(APIView):
 
-    def patch(self, request, album_id):
-        pass
+    def put(self, request, album_id, photo_id):
+        user, data = request.user, request.data
 
-    def put(self, request, album_id):
-        pass
+        album = get_object_or_404(models.Album, id=album_id, user=user)
+        photo = get_object_or_404(models.Photo, id=photo_id, user=user)
 
-    # def patch(self, request, album_id):
-    #     user = request.user
-    #     data = request.data
-    #     serialzier = serializer.
+        album.photos.add(photo)
+
+        serializer = serializers.AlbumSerializer(album)
+        return Response(serializer.data,
+                        status=rest_status.HTTP_200_OK)
 
 # Create album has photos as optional
 # retrieve album has all the photo information (at least URL) Do not need to solve n+1 join
