3d6970865fcd1141b39862008925eb6f86423e26
Alan Chang <ahchang012@gmail.com>
2021-07-08 17:20:20 -0700

    Include photo as a subobject list of the album object.

--- photo_share/serializers.py
+++ photo_share/serializers.py
@@ -2,18 +2,19 @@ from rest_framework import serializers
 from . import models
 
 
-class AlbumSerializer(serializers.ModelSerializer):
-    photos = serializers.PrimaryKeyRelatedField(required=False, many=True)
-
-    class Meta:
-        model = models.Album
-        fields = ['name', 'creation_date', 'photos']
-
-
 class PhotoSerializer(serializers.ModelSerializer):
-
 
 
     class Meta:
         model = models.Photo
-        fields = ['s3_file_url', 'creation_date']
+        fields = ['s3_file_url', 'creation_date', 'id']
+
+
+class AlbumSerializer(serializers.ModelSerializer):
+    photos = PhotoSerializer(required=False, many=True, read_only=True)
+
+
+    class Meta:
+        model = models.Album
+        fields = ['name', 'creation_date', 'photos', 'id']
+        depth = 1
